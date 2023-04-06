8d912214fbe45a214ecc044942a9b81da3286fc1
Creepy0964 <59210160+Creepy0964@users.noreply.github.com>
2023-04-06 22:27:43 +0300

    Add files via upload

---
+++ aspects/index.jsx
@@ -0,0 +1,119 @@
+const colors = {
+    etonBlue: '#86cb92',
+    shinyShamrock: '#FAF3DD',
+    purpleNavy: '#8FC0A9',
+    spaceCadet: '#FAF3DD',
+    darkPurple: '#696D7D'
+};
+
+const styleObj = {
+    color: colors.spaceCadet,
+    fontFamily: 'Segoe UI Semibold',
+    backgroundColor: colors.purpleNavy,
+    marginTop: '-20px',
+    marginBottom: '-20px',
+    marginLeft: '-5px',
+    width: '100.7%'
+};
+
+const mainStyle = {
+    color: colors.shinyShamrock,
+    fontFamily: 'Segoe UI Semibold',
+}
+
+const textStyle = {
+    marginTop: '-20px',
+}
+
+const final = {
+    position: 'absolute',
+    bottom: '0%'
+}
+
+const headerButtons = {
+    border: 'none',
+    borderRadius: '7px',
+    fontFamily: 'Segoe UI Semibold',
+    fontSize: '15px',
+    height: '35px',
+    width: '150%',
+    backgroundColor: colors.shinyShamrock,
+};
+
+const test = {
+    float: 'left',
+    position: 'absolute',
+    right: '350px',
+    top: '5px',
+}
+
+const test1 = {
+    float: 'left',
+    position: 'absolute',
+    right: '254px',
+    top: '5px',
+}
+
+const test2 = {
+    float: 'left',
+    position: 'absolute',
+    right: '153px',
+    top: '5px',
+}
+
+const test3 = {
+    float: 'left',
+    position: 'absolute',
+    right: '41px',
+    top: '5px',
+}
+
+function index() {
+    document.location = '/';
+}
+
+function theory() {
+    document.location = '/theory/index.html';
+}
+
+function aspects() {
+    document.location = '/aspects/index.html';
+}
+
+function contacts() {
+    document.location = '/contacts/index.html';
+}
+
+ReactDOM
+.createRoot(document.getElementById("app"))
+.render(
+    <div style={styleObj}>
+        <h1>Психософия - помощь в самотипировании</h1> 
+        <div style={test}>
+            <button style={headerButtons} onClick={index}>Главная</button> 
+        </div> 
+        <div style={test1}>
+            <button style={headerButtons} onClick={theory}>Теория</button>   
+        </div> 
+        <div style={test2}>
+            <button style={headerButtons} onClick={aspects}>Аспекты</button>   
+        </div> 
+        <div style={test3}>
+            <button style={headerButtons} onClick={contacts}>Контакты</button>   
+        </div> 
+    </div>    
+);
+
+ReactDOM
+.createRoot(document.getElementById("main"))
+.render(
+    <div style={mainStyle}>
+        <h1>Аспекты</h1>
+        <p style={textStyle}>Чтобы перейти на страницу интересующего Вас аспекта, перейдите по ссылкам ниже:</p>
+        <a style={mainStyle} href="will/index.html">Воля</a><br />
+        <a style={mainStyle} href="logic/index.html">Логика</a><br />
+        <a style={mainStyle} href="physics/index.html">Физика</a><br />
+        <a style={mainStyle} href="emotion/index.html">Эмоция</a><br />
+         
+    </div>    
+);
